f953a84829420097e806cb69c2f151323b271924
Eduardo DuQuesne <eduquesne@gmail.com>
2018-05-14 21:10:17 -0500

    Add Tempo

--- client/src/components/SubmitAnswers.js
+++ client/src/components/SubmitAnswers.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Audio from './Audio';
+import SelectTempo from './SelectTempo';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import '../css/submitAnswers.css';
 
 class SubmitAnswers extends Component {
+  
   
   render() {
     let isSubmitted = this.props.submitted;
@@ -16,12 +18,17 @@ class SubmitAnswers extends Component {
               <RaisedButton onClick={this.props.nextQuestion} className={`${!isSubmitted ? "hide-btn" : ""}`} label="Next"  />
             </div>
           </MuiThemeProvider>
-          <Audio 
+          <Audio
             toneProgression={this.props.toneProgression}
-            setStatePlay={this.props.setStatePlay} 
-            setStateStop={this.props.setStateStop} 
+            playProgression={this.props.playProgression}
+            stopProgression={this.props.stopProgression}
             playing={this.props.playing}
-          />        
+          />  
+          <div>      
+            <SelectTempo
+              changeTempo={this.props.changeTempo}
+              />
+          </div>
         </div>
       
     );
